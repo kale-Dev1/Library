f4ca8531ed808a98f31971a6f29cad5ddc64cd0a
Kale Kamara <102758341+kale-Dev1@users.noreply.github.com>
2022-12-08 17:59:53 +0000

    Add Class to table body

--- script.js
+++ script.js
@@ -1,3 +1,33 @@
+const container = document.querySelector(".main");
+const table = document.querySelector(".tbody");
+const tbrow = document.createElement('tr')
+const tbroh = document.createElement('th')
+let addbutton = document.querySelector("btn-primary")
+
+const form = document.querySelector('form');
+
+function addToLibrary(e){
+    e.preventDefault();
+    alert('hello')
+
+}
+form.addEventListener('submit', addToLibrary)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 let myLibrary =[];
 
 function Book(title, author, pages){
@@ -20,7 +50,14 @@ const book1 = new Book('JJ','Rowling', 96);
 book1.info();
 addToLibray(book1)
 
-const container = document.querySelector(".main");
+
+tbroh.classList.add("row")
+
+
+table.append(tbrow);
+
+
+
 
 
 console.log(myLibrary)
