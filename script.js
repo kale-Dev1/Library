10e402a2c88b4275a46a7dba4f58d81ea8101417
Kale Kamara <102758341+kale-Dev1@users.noreply.github.com>
2022-12-11 07:14:11 +0000

    Add Function to READ check mark

--- script.js
+++ script.js
@@ -8,6 +8,7 @@ let tableRead = document.querySelector(".reado")
 const form = document.querySelector('form');
 const check = document.querySelector('.fa-check')
 const btn2 = document.querySelector('.deletebtn2');
+const readButton = document.querySelector('.readOrNot')
 
 function addToLibrary(e){
     e.preventDefault();
@@ -28,12 +29,13 @@ function addToLibrary(e){
     <th scope="row">${title}</th>
     <td>${author}</td>
     <td>${pages}</td>
-    <td>read</td>
-    <td><button class = "deletebtn"> Delete</button></td>
+    <td class="reado"><button class="readOrNot" type="button" aria-label='delete item' ><i class="fa fa-check" aria-hidden="true"></i></button></td>
+    <td><button class = "deletebtn"> <i class="fa fa-trash" aria-hidden="true"></i></button></td>
     
     `
 }
 
+// Deletet row when delete button is pressed
 function deleteRow(e){
     if(!e.target.classList.contains("deletebtn")){
         return;
@@ -44,37 +46,51 @@ function deleteRow(e){
 }
 form.addEventListener('submit', addToLibrary)
 table.addEventListener('click', deleteRow)
+table.addEventListener('click', bookRead)
 
+// Check if book is read or not
+function bookRead(e){
 
-function hello(){
-    if(check.classList.contains('fa-check')){
-        check.classList.replace('fa-check','fa-times');
+    // if(!e.target.classList.contains("fa-check")){
+    //     return;
+    // }
+    const readbtn = e.target;
+    if(readbtn.classList.contains('fa-check')){
+        readbtn.classList.replace('fa-check','fa-times');
+        return;
     }else{
-        check.classList.replace('fa-times','fa-check');
+        readbtn.classList.replace('fa-times','fa-check');
         
     }
-   
 }
+   
+    
+    
 
-(function () {
-    'use strict'
+    
+ 
+   
+
+
+// (function () {
+//     'use strict'
   
-    // Fetch all the forms we want to apply custom Bootstrap validation styles to
-    var forms = document.querySelectorAll('.needs-validation')
+//     // Fetch all the forms we want to apply custom Bootstrap validation styles to
+//     var forms = document.querySelectorAll('.needs-validation')
   
-    // Loop over them and prevent submission
-    Array.prototype.slice.call(forms)
-      .forEach(function (form) {
-        form.addEventListener('submit', function (event) {
-          if (!form.checkValidity()) {
-            event.preventDefault()
-            event.stopPropagation()
-          }
+//     // Loop over them and prevent submission
+//     Array.prototype.slice.call(forms)
+//       .forEach(function (form) {
+//         form.addEventListener('submit', function (event) {
+//           if (!form.checkValidity()) {
+//             event.preventDefault()
+//             event.stopPropagation()
+//           }
   
-          form.classList.add('was-validated')
-        }, false)
-      })
-  })()
+//           form.classList.add('was-validated')
+//         }, false)
+//       })
+//   })()
 
 
 
